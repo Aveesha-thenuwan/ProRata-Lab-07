27fde69e1ec90487d19e20b7a6184c9fd8ce2538
Aveesha-thenuwan <it23362130@my.sliit.lk>
2026-09-17 15:29:43 +0530

    IT23362130Lab7Q2A.java, IT23362130Lab7Q2B.java, IT23362130Lab7Q2C.java

---
+++ Q2/IT23362130Lab7Q2B.java
@@ -0,0 +1,11 @@
+public class IT23362130Lab7Q2B {
+    public static void main(String[] args) {
+        for (int row = 1; row <= 5; row++) {
+            System.out.print(row + " - ");
+            for (int star = 1; star <= row; star++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
